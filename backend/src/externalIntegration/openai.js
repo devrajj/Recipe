d07fb14813f7a2073a6f3e74a7905160b18328c1
devraj.sen <devraj.sen@uolo.com>
2024-02-02 02:22:03 +0530

    added image generation flow with basic testing

--- backend/src/externalIntegration/openai.js
+++ backend/src/externalIntegration/openai.js
@@ -4,9 +4,27 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const generateImage = async ({ recipeText }) => {
+  try {
+    // we can change the models and other options of image generation from here
+    const data = await openai.images.generate({
+      model: "dall-e-2",
+      prompt: recipeText,
+      n: 1,
+      quality: "standard",
+      response_format: "url",
+    });
+    return data.data[0].url;
+  } catch (err) {
+    console.error("err:", err.stack);
+    return "";
+  }
+};
+
 module.exports = {
   createPrompt: async ({ userPrompt }) => {
     try {
+      // we can change the models and other options of completions from here
       const data = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [
@@ -20,9 +38,15 @@ module.exports = {
           },
         ],
       });
+      const text = data.choices[0].message.content;
+      const recipeText = data.choices[0].message.content.trim().slice(0, 1000);
+      const file = await generateImage({ recipeText });
       return {
         ok: true,
-        data: data.choices[0].message.content,
+        data: {
+          text,
+          file,
+        },
       };
     } catch (err) {
       console.error("Error in createPrompt:", err.stack);
